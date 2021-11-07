7b35a598b976638680986fa597255ba7818c6c3a
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-11-07 20:51:25 +0300

    Add files via upload

---
+++ lr_1/lr_1/MyTask.java
@@ -0,0 +1,46 @@
+package lr_1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyTask {
+
+	public static void DoTask() 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("¬ведите количество чисел > ");
+		int N = sc.nextInt();
+		
+		double a;
+		ArrayList<Double> aa = new ArrayList();
+		
+		for(int i=0; i<N; i++) 
+		{
+			System.out.print("¬ведите число "+String.valueOf(i+1)+" > ");
+			a = sc.nextDouble();
+			aa.add(a);
+		}
+		
+		double max = aa.get(0);
+		for (Double i: aa) {
+		    if(i > max) 
+		        max = i;
+		}
+		System.out.println(max);
+		
+		double max2 = aa.get(0);
+		for (Double i: aa) {
+			if(i != (max-1)) 
+				if (i > max2)
+		    	max2 = i;
+		}
+		System.out.println(max2);
+		
+		double b = 0;
+		System.out.println("—умма квадратов = ");
+		b = max*max + max2*max2;
+		System.out.println(b);
+		sc.close();
+	}
+
+}
---
+++ lr_1/lr_1/Starter.java
@@ -0,0 +1,10 @@
+package lr_1;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		MyTask.DoTask();
+
+	}
+
+}
