854fad830f0cb2fccfc2f75a37c935ceb1cc978a
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 01:15:49 +0300

    Update Shkaf.java

--- lr4/lr4/Shkaf.java
+++ lr4/lr4/Shkaf.java
@@ -1,5 +1,16 @@
 package lr4;
 
+
 public class Shkaf {
+	float vysota; 
+	String tsvet;
+	
+	public Shkaf (String c, String m, String n, float p, float vys, String t) 
+	{
+		super(c, m, n, p);
+		vysota =vys;
+		tsvet = t;
+	}
+	
 
 }
