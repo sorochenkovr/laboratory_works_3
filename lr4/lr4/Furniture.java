11150d530d16ff85f9d6478761c2fbaf52aa8f0c
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 01:15:31 +0300

    Update Furniture.java

--- lr4/lr4/Furniture.java
+++ lr4/lr4/Furniture.java
@@ -1,5 +1,27 @@
 package lr4;
 
+
 public class Furniture {
+	protected String cod;
+	protected String material; 
+	protected String naming;
+	protected float price;
+	
+	public Furniture() {}
+	public Furniture (String c, String m, String n, float p)
+	{
+		cod = c;
+		material = m;
+		naming = n;
+		price = p;
+	}
+	
+	@Override 
+	public String toString()
+	{
+		return "Индивидуальный код мебели: " + cod + "\n Из материала: " + material 
+				+ "\n Под названием: " + naming + "\n Цена: " + String.valueOf(price);
+	}
+
 
 }
