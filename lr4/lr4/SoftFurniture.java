81755e7f4dcc391c4a66c2df4eedc4cf168a874f
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 01:16:03 +0300

    Update SoftFurniture.java

--- lr4/lr4/SoftFurniture.java
+++ lr4/lr4/SoftFurniture.java
@@ -1,5 +1,15 @@
 package lr4;
 
+import java.util.GregorianCalendar;
+
 public class SoftFurniture {
+String obivka; 
+	
+	public SoftFurniture(String c, String m, String n, float p, String obiv) 
+	{
+		super(c, m, n, p);
+		obivka =obiv;
+	}
+	
 
 }
