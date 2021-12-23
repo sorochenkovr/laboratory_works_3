3a1689c32ea61c912e1135d02e7006e02a00af08
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 14:40:09 +0300

    Update Mashiny.java

--- lr6/lr6/Mashiny.java
+++ lr6/lr6/Mashiny.java
@@ -1,5 +1,24 @@
 package lr6;
+import java.util.Vector;
 
 public class Mashiny {
+
+	protected String type;
+	protected int probeg;
+	
+
+	
+	public void Worker(String TIP_MASHINY, int PROBEG)
+	{
+		type = TIP_MASHINY;
+		probeg = PROBEG;
+	}
+	@Override
+	public String toString()
+	{
+		return "Марка: " + type + "\n" + 
+			   "Пробег (в годах): " + probeg + "\n";
+	}
+	
 
 }
