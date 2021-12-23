8bdc286a948e1022994fdd93252daa592cdef792
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 14:43:07 +0300

    Update FrameWindow.java

--- lab_8/src/lab_8/FrameWindow.java
+++ lab_8/src/lab_8/FrameWindow.java
@@ -16,18 +16,18 @@ import javax.swing.JTextField;
 public class FrameWindow extends JFrame {
 
 	public FrameWindow(int axis)
-	{super("Вариант 25");
+	{super("Р’Р°СЂРёР°РЅС‚ 25");
 	setSize(500,300);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	Container cont = getContentPane();
 	BoxLayout manager = new BoxLayout(cont, axis);
 	cont.setLayout(manager);
-	cont.add(new JLabel("Выберите выражение: "));
-	JRadioButton var1 = new JRadioButton("Выражение 1");
+	cont.add(new JLabel("Р’С‹Р±РµСЂРёС‚Рµ РІС‹СЂР°Р¶РµРЅРёРµ: "));
+	JRadioButton var1 = new JRadioButton("Р’С‹СЂР°Р¶РµРЅРёРµ 1");
 	cont.add(var1);
-	JRadioButton var2 = new JRadioButton("Выражение 2");
+	JRadioButton var2 = new JRadioButton("Р’С‹СЂР°Р¶РµРЅРёРµ 2");
 	cont.add(var2);
-	JButton btn = new JButton("Подсчитать");
+	JButton btn = new JButton("РџРѕРґСЃС‡РёС‚Р°С‚СЊ");
 	JTextField answer = new JTextField("0");
 	cont.add(answer);
 	btn.addActionListener(new ActionListener()
@@ -37,14 +37,14 @@ public class FrameWindow extends JFrame {
 				{
 					if (var1.isSelected())
 					{
-						Dlg1 d1 = new Dlg1(null, "Выражение 1", true, BoxLayout.Y_AXIS);
+						Dlg1 d1 = new Dlg1(null, "Р’С‹СЂР°Р¶РµРЅРёРµ 1", true, BoxLayout.Y_AXIS);
 						d1.setVisible(true);
 						answer.setText(String.valueOf(d1.Variant1(d1.n, d1.x)));
 						
 					}
 					else if(var2.isSelected())
 					{
-						Dlg2 d2 = new Dlg2(null, "Выражение 2", true, BoxLayout.Y_AXIS);
+						Dlg2 d2 = new Dlg2(null, "Р’С‹СЂР°Р¶РµРЅРёРµ 2", true, BoxLayout.Y_AXIS);
 						d2.setVisible(true);
 						answer.setText(String.valueOf(d2.Variant2(d2.getN(), d2.getR())));
 						
