70dae32de0ec41c069a53a4f7aae0150b6098930
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 14:39:09 +0300

    Update Hyundai.java

--- lr6/lr6/Hyundai.java
+++ lr6/lr6/Hyundai.java
@@ -1,5 +1,34 @@
 package lr6;
 
-public class Hyundai {
+ import lr6.Hyndai_enum;
+ public class Hyundai implements IDraw{
+
+	 Hyndai_enum hyun;
+	public Hyundai() {}
+	public Hyundai (Hyndai_enum h)
+	{
+		hyun = h;
+	}
+	
+
+	
+	public void Draw()
+	{
+		System.out.println("      _________           \n "
+				  +"   __|         |___       \n "
+				  +"  |             ___|      \n "
+				  +"     (_)        (_)        \n ");
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Направление: " + String.valueOf(hyun) + "\n" ;
+	}
+	@Override
+	public String MASHINE() {
+			return "Менеджер";
+		
+	}
 
 }
