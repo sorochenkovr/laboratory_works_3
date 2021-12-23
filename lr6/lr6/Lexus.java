dd8ff0d00544ebf808c37b8226387055a156e570
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-12-23 14:39:42 +0300

    Update Lexus.java

--- lr6/lr6/Lexus.java
+++ lr6/lr6/Lexus.java
@@ -1,5 +1,32 @@
 package lr6;
+import lr6.lexus_enum;
 
-public class Lexus {
+public class Lexus implements IDraw{
 
+	lexus_enum lex;
+	public Lexus() {}
+	public Lexus (lexus_enum l)
+	{
+		lex = l;
+	}
+	
+
+	
+	public void Draw()
+	{
+		System.out.println("      _________           \n "
+				  +"   __|         |___       \n "
+				  +"  |             ___|      \n "
+				  +"     (_)        (_)        \n ");
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Направление: " + String.valueOf(lex) + "\n" ;
+	}
+	@Override
+	public String MASHINE() 	{
+		return "Менеджер";
+	}
 }
